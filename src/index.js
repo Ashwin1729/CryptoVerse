3cfde7bb1f29470f02a025af9e4b0315a16454b6
Ashwin1729 <mathematicswroteus@gmail.com>
2022-04-20 01:55:45 +0530

    Layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { BrowserRouter } from "react-router-dom";
+import "antd/dist/antd.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.createRoot(rootElement).render(<App />);
+ReactDOM.createRoot(rootElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
