8b7dd355ce8df94a7b1eddcb508b1476e38a3fdd
Ashwin1729 <mathematicswroteus@gmail.com>
2022-05-14 01:35:15 +0530

    functionality

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "antd/dist/antd.css";
 import App from "./App";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.createRoot(rootElement).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/coins",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     "tiers[0]": "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {
+//     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+//     "X-RapidAPI-Key": "a0482efaddmsh96008daf6627c99p1bfb1bjsn897fc89565d1",
+//   },
+// };
